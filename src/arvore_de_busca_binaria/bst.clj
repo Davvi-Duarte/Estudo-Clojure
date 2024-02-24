827f7fa0c9e1f319cd20c294f6d13a0aa7cf966c
Davvi Duarte Rodrigues <davvi.duarte@gmail.com>
2024-02-23 22:44:05 -0300

    Update bst.clj
    depois de muito esforço, consegui criar os nós com os valores de parents. como estou muito cansado estarei comentando mais profundamente posteriormente

--- src/arvore_de_busca_binaria/bst.clj
+++ src/arvore_de_busca_binaria/bst.clj
@@ -2,28 +2,27 @@
   (:require [clojure.string :as str]))
 
 
-(defrecord Node [valor esquerda direita])
+(defrecord Node [valor esquerda direita parent])
 
 (defn criar-node
   "funcao que cria um node"
-  [valor]
-  (Node. valor nil nil ))
+  [valor parent]
+  (Node. valor nil nil parent))
 
 (defn inserir-node
   "docstring"
-  [raiz valor]
+  [raiz valor parent]
   (if (nil? raiz)
-    (criar-node valor)
+    (criar-node valor parent)
     (if (< valor (:valor raiz))
-      (assoc raiz :esquerda (inserir-node (:esquerda raiz) valor))
-      (assoc raiz :direita (inserir-node (:direita raiz) valor))))
-  )
+      (assoc raiz :esquerda (inserir-node (:esquerda raiz) valor raiz))
+      (assoc raiz :direita (inserir-node (:direita raiz) valor raiz)))))
 
 (defn criar-arvore
   "funcao que monta uma arvore binaria"
   [valores]
-  (reduce inserir-node nil valores)
-  )
+  (reduce (fn [tree valor] (inserir-node tree valor nil)) nil valores))
+
 
 ;teste
 (def arvore-teste (criar-arvore [5 3 8 7 2 4 1 6]))
@@ -36,4 +35,19 @@
     (imprimir-arvore (:direita node) (inc nivel))
     (println (apply str (repeat nivel "    ")) (:valor node))
     (imprimir-arvore (:esquerda node) (inc nivel))))
-(println (imprimir-arvore arvore-teste 0) )
+(println (imprimir-arvore arvore-teste 0))
+
+
+(defn imprimir-desc-arvore
+  "Imprime os nós da árvore, incluindo valor, nó da esquerda, nó da direita e nó pai."
+  [node]
+  (when node
+    (println (format "Valor: %s, Esquerda: %s, Direita: %s, Pai: %s"
+                     (:valor node)
+                     (if (:esquerda node) (:valor (:esquerda node)) nil)
+                     (if (:direita node) (:valor (:direita node)) nil)
+                     (if (:parent node) (:valor (:parent node)) nil)))
+    (imprimir-desc-arvore (:esquerda node))
+    (imprimir-desc-arvore (:direita node))))
+
+(println (imprimir-desc-arvore arvore-teste))
